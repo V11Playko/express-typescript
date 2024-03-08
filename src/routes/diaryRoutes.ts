4302235ee7b8504a0c176481bb25b2ea02c2007e
Heinner Vega <heinnervega20@gmail.com>
2024-03-08 16:26:14 -0500

    Add endpoint findById

--- src/routes/diaryRoutes.ts
+++ src/routes/diaryRoutes.ts
@@ -1,13 +1,20 @@
-import express from 'express'
+import express, { json } from 'express'
+import * as diaryServices from '../services/diaryServices'
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-    res.send('Fetching')
+    res.send(diaryServices.getEntriesWithoutSensitiveInfo())
 })
 
-router.post('/', (_req, res) => {
-    res.send('Saving')
+router.get('/:id', (req, res) => {
+    const diary = diaryServices.findById(Number(req.params.id))
+
+    return (diary !== null)
+    ? res.send(diary)
+    : res.sendStatus(404)
 })
+
+
 
 export default router
--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,10 +1,26 @@
-import { DiaryEntry, NonSensitiveInfoDiaryEntry } from '../types.js'
+import { DiaryEntry, NonSensitiveInfoDiaryEntry, Visibility, Weather } from '../types.js'
 import diaryData from './diary.json'
 
 const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>
 
 export const getEntries = () => diaries
 
-export const getEntriesWithoutSensitiveInfo= (): NonSensitiveInfoDiaryEntry[] => diaries
+export const findById = (id: number): NonSensitiveInfoDiaryEntry | undefined => {
+    const entry = diaries.find(d => d.id === id);
+    if (entry) {
+        const { comment, ...entryWithoutComment } = entry;
+        return entryWithoutComment;
+    }
+    return undefined;
+}
 
-export const addEntry = () => null 
+export const getEntriesWithoutSensitiveInfo= (): NonSensitiveInfoDiaryEntry[] => {
+    return diaries.map(({id, date, weather, visibility}) =>{
+        return {
+            id,
+            date,
+            weather,
+            visibility
+        }
+    })
+}
